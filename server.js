cd5982419ec5ab754c067aa36d199fd8a60122ef
Christiana <Christiana@Christianas-MacBook-Air.local>
2026-01-27 15:25:04 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+// Imports
+
+
+// Setups
+const app = express();
+const PORT = 3000;
+
+// (Request) Middleware
+
+// Routes
+
+// Global Error Handling Middleware (err, req, res, next)
+
+
+// Listener
